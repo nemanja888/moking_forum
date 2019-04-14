3dbdf0eeda80bcee990a0b5c77ff04e5916f53b2
Nemaja Jekic <nemanja.develop@gmail.com>
2019-04-14 14:56:14 +0200

    before unstasing

---
+++ resources/js/components/AppHome.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <toolbar></toolbar>
+<!--        <router-view></router-view>-->
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+import Toolbar from './Toolbar';
+import Footer from './AppFooter';
+
+    export default {
+        name: "AppHome",
+        components: {
+            toolbar: Toolbar,
+            appFooter: Footer,
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/Toolbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-toolbar>
+        <v-toolbar-side-icon></v-toolbar-side-icon>
+        <v-toolbar-title>Moking Forum</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+<!--            <route-link to="/question">-->
+<!--                <v-btn flat>Forum</v-btn>-->
+<!--            </route-link>-->
+
+            <v-btn flat>Ask Question</v-btn>
+            <v-btn flat>Categories</v-btn>
+            <v-btn flat>Log In</v-btn>
+        </v-toolbar-items>
+    </v-toolbar>
+</template>
+
+<script>
+    export default {
+        name: "Toolbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
